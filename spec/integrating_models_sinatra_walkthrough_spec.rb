6321082537e9255e13b66c677a7c3693323735ab
Rauof M <abdelrauof197@gmail.com>
2018-02-18 07:12:33 +0000

    automatically backed up by learn

--- spec/integrating_models_sinatra_walkthrough_spec.rb
+++ spec/integrating_models_sinatra_walkthrough_spec.rb
@@ -6,25 +6,25 @@ describe App do
       expect(last_response.status).to eq(200)
     end
 
-   # it 'renders form' do
-    #  visit '/'
-     # expect(page).to have_selector("form")
-    #  expect(page).to have_field(:user_text)
-  #  end
+    it 'renders form' do
+      visit '/'
+      expect(page).to have_selector("form")
+      expect(page).to have_field(:user_text)
+    end
   end
 
-  #describe 'POST /' do
-   # it "displays string results" do
-    #  visit '/'
-#
- #     #fill_in(:user_text, :with => "Green Eggs and Ham")
-      #click_button "submit"
-     # expect(page).to have_text("Number of Words: 4")
-    #  expect(page).to have_text("Vowels: 5")
-   #   expect(page).to have_text("Consonants: 10")
-  #    expect(page).to have_text("Most Common Letter: G, used 3 #times")
-    #end
-  #end
+  describe 'POST /' do
+    it "displays string results" do
+      visit '/'
+
+      fill_in(:user_text, :with => "Green Eggs and Ham")
+      click_button "submit"
+      expect(page).to have_text("Number of Words: 4")
+      expect(page).to have_text("Vowels: 5")
+      expect(page).to have_text("Consonants: 10")
+      expect(page).to have_text("Most Common Letter: G, used 3 times")
+    end
+  end
 
   describe 'TextAnalyzer Class' do
     let!(:words) { TextAnalyzer.new("mirror mirror on the wall") }
